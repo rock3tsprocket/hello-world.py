740205fd27003ec018e97c8a6e1257e9b5df5e92
rock3tsprocket <rock-pie@hotmail.com>
2026-04-30 21:22:57 +0100

    Initial commit

---
+++ hello.py
@@ -0,0 +1,29 @@
+import sys;
+
+class Main:
+    def putc(self, char: chr) -> int:
+        return sys.stdout.write(char)
+
+    def puts(self, string: str) -> int:
+        amount_of_bytes_written: int = 0;
+
+        for i in range(0, len(string)):
+            amount_of_bytes_written = amount_of_bytes_written + self.putc(string[i]);
+
+        return amount_of_bytes_written+1;
+
+    def _main(self) -> int:
+        hello: list = [None, None, None, None, None, None, None, None, None, None, None, None, None];
+        for i in range(0, 13):
+            hello[i] = "Hello, world!"[i];
+
+        for i in range(0, 13):
+            self.puts(hello[i]);
+        self.putc(chr(13)+chr(10));
+        return 0;
+
+    def ___PosixProcessStartup(self) -> None:
+        self._main();
+
+if __name__ == "__main__":
+    sys.exit(Main()._Main___PosixProcessStartup());
