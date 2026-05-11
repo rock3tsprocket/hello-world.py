6e65eb024b4ff7dafeb62a0748548c332f381189
rock3tsprocket <rock-pie@hotmail.com>
2026-05-11 12:35:58 +0100

    forgot semicolons in some places

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ import sys;
 
 class Main:
     def putc(self, char: str) -> int:
-        return sys.stdout.write(char[0])
+        return sys.stdout.write(char[0]);
 
     def puts(self, string: str) -> int:
         amount_of_bytes_written: int = 0;
@@ -19,7 +19,7 @@ class Main:
 
         for i in range(0, 13):
             self.puts(hello[i]);
-        self.putc(chr(13))
+        self.putc(chr(13));
         self.putc(chr(10));
         return 0;
 
